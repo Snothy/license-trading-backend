61b59f0dde9b0c87e7dcc0cb4663ca6099826b75
Petar Drumev <drumevp@coventry.ac.uk>
2021-05-14 16:12:26 +0000

    Update roles route functions
    Add concrete function placeholders for roles route.

--- models/roles.js
+++ models/roles.js
@@ -1,2 +1,25 @@
 const db = require('../helpers/database');
 
+exports.getAllRoles = async function getAllRoles() {
+    return null;
+}
+
+exports.createRole = async function createRole(role) {
+    return null;
+}
+
+exports.updateRole = async function updateRole(role) {
+    return null;
+}
+
+exports.removeRole = async function removeRole(role_id) {
+    return null;
+}
+
+exports.assignUserRole = async function assignUserRole(user_id, role_id) {
+    return null;
+}
+
+exports.removeUserRole = async function removeUserROle(user_id, role_id) {
+    return null;
+}
--- routes/roles.js
+++ routes/roles.js
@@ -2,6 +2,17 @@ const Router = require('koa-router');
 const bodyparser = require('koa-bodyparser');
 const model = require('../models/users');
 
-const router = Router({prefix : '/api/users'});
+//route only accessible by the admin role | manages staff/users
+const router = Router({prefix : '/api/roles'});
 
-//Create, modify, remove, assign role to user, modify user roles
+router.get('/', getAllRoles);                               //list all roles
+router.post('/', bodyparser(), createRole);                 //create new roles
+
+router.put('/:id([0-9]{1,})', bodyparser(), updateRole);    //update role
+router.del('/:id([0-9]{1,})', bodyparser(), removeRole);    //remove role
+
+router.get('/roles', getUsersRoles);                                //list all users and their roles, once a user is selected (:/id), can perform bottom actions on them                                  
+router.post('/roles/:id([0-9]{1,})', bodyparser(), assignUserRole); //create new entry in users_roles table (assign role) using user id
+router.del('/roles/:id([0-9]{1,})', bodyparser(), removeUserRole);  //remove entry from users_roles table (remove role) using user id
+
+
