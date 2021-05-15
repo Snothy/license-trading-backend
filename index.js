889a8f31e7d9737af65ffce6c30e66896551da81
Petar Drumev <drumevp@coventry.ac.uk>
2021-05-15 00:31:19 +0000

    Implement logic for chat route
    Write functionality for chat route. Requires merge with master for proper function, as it depends on currently unimplemented functions.

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = new Koa();
 const users = require('./routes/users.js');
 const shelters = require('./routes/shelters.js')
 const dogs = require('./routes/dogs.js');
+const chat = require('./routes/chat.js');
 
 app.use(users.routes());
 app.use(shelters.routes());
 app.use(dogs.routes());
+app.use(chat.routes());
 
 let port = process.env.PORT || 3000;
 
