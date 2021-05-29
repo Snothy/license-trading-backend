8c519dfc30ad1cc49a3052d8c12ee94788667522
Petar Drumev <drumevp@coventry.ac.uk>
2021-05-29 16:26:58 +0000

    Implement JSDoc documentation
    Install JSDoc. Create jsdoc configuration file. Document all functions using JSDoc standard notation.
    
    Remove server.js file as it no longer servers a purpose.

--- helpers/database.js
+++ helpers/database.js
@@ -1,6 +1,20 @@
+/**
+ * Module that runs SQL queries on a specified DB.
+ * @module controllers/validation
+ * @author Petar Drumev
+ * @see models/* for models that make use of this module
+ */
+
 const mysql = require('promise-mysql');
 const info = require('../config');
 
+/**
+ * Run an SQL query on the Database.
+ * @param {string} query - The SQL query string
+ * @param {string} values - The values that replace the placeholders in the query
+ * @returns {object} - The mysql response object - either data we can index or information about the executed query
+ * @throws {error} - Error from the mysql library representing what went wrong with the query execution
+ */
 exports.run_query = async function run_query (query, values) {
     try {
         const connection = await mysql.createConnection(info.config);
